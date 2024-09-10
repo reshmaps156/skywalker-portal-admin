7a6d82bed6bd084071d6cbf5b6a4e187ab70e124
Reshma PS <reshmaps156@gmail.com>
2024-09-10 20:53:52 +0530

    sidebar placed in admin's dashboard as a nested component

--- src/pages/Adminpanel.jsx
+++ src/pages/Adminpanel.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import SideControls from '../components/SideControls'
 
 function Adminpanel() {
   return (
-    <div>Adminpanel</div>
+    <>
+    <div className="container-fluid w-100 m-0 p-0 d-flex" >
+      <SideControls/>
+      <div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt placeat explicabo accusamus, laudantium tempora eius facere ullam cumque necessitatibus expedita rem rerum aut alias hic corrupti recusandae molestias est quia sequi! Id, saepe explicabo? Id aperiam molestiae ducimus at, quaerat possimus esse fuga, consequatur est debitis voluptatum eum libero dolorum porro repellat perferendis et dolores itaque, nemo expedita sit voluptas. Quos laboriosam quas eaque tempore facilis rerum unde sequi quo asperiores excepturi beatae consequuntur aliquam voluptates alias deserunt error et, maxime qui suscipit temporibus soluta placeat! Quis, animi id, ea quas assumenda praesentium sapiente unde omnis amet eos laboriosam eius.</p>
+      </div>
+    </div>
+    </>
   )
 }
 
