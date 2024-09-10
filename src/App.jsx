58ee9bd6dc9d5b48cf1ce4dbf39eb0561294e621
Reshma PS <reshmaps156@gmail.com>
2024-09-10 21:41:52 +0530

    protected path provided for admin dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import Adminpanel from './pages/Adminpanel'
+import ProtectedRoutes from './utils/ProtectedRoutes'
 
 function App() {
   
@@ -10,7 +11,9 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route element={<ProtectedRoutes/>}>
         <Route path='/admin' element={<Adminpanel/>}/>
+        </Route>
       </Routes>
        
     </>
