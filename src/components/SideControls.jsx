b11058df5b162b872531a2f24b1491d0bda5c59b
Reshma PS <reshmaps156@gmail.com>
2024-09-11 23:10:01 +0530

    chart added in admin panel

--- src/components/SideControls.jsx
+++ src/components/SideControls.jsx
@@ -1,7 +1,7 @@
-import { faBars, faPowerOff } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRightFromBracket, faBoxesStacked, faInbox, faJedi } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react'
-import { Sidebar, Menu, MenuItem} from 'react-pro-sidebar';
+import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
 
 function SideControls() {
     const [isCollapsed, setIsCollpased] = useState(false)
@@ -24,19 +24,19 @@ function SideControls() {
         }
     }, [])
     return (
-        <Sidebar collapsed={isCollapsed } backgroundColor='black'  className='shadow h-100 border-0'>
-            <FontAwesomeIcon icon={faBars} onClick={handleCollpase} size='2xl ps-4 mt-4 mb-3 text-warning'/>
+        <Sidebar collapsed={isCollapsed} backgroundColor='#101010' className='shadow h-100 border-0' >
+            <div className=' border border-4 border-warning rounded-circle d-flex align-items-center justify-content-center ms-3 my-4 ' style={{ width: '40px', height: '40px', cursor: 'pointer' }} onClick={handleCollpase}>
+                <FontAwesomeIcon icon={faJedi} size='xl  text-warning' />
+            </div>
             <Menu menuItemStyles={{
-                button:{
-                    // "color": "yellow",
-                    ":hover": { backgroundColor: 'grey', color: 'white' }
+                button: {
+                    
+                    ":hover": { backgroundColor: '#2b2b2b', color: 'white', borderEndEndRadius:'20px',borderTopRightRadius:'20px'}
                 }
             }}>
-                
-                <MenuItem> Documentation </MenuItem>
-                <MenuItem> Calendar </MenuItem>
-                <MenuItem> Calendar </MenuItem>
-                <MenuItem ><FontAwesomeIcon icon={faPowerOff} size='xl' className='text-warning me-3'/> Logout </MenuItem>
+                <MenuItem> <FontAwesomeIcon icon={faBoxesStacked} size='xl' className=' me-3 ' />Dashboard </MenuItem>
+                <MenuItem> <FontAwesomeIcon icon={faInbox} size='xl' className=' me-3 ' />Inbox </MenuItem>
+                <MenuItem ><FontAwesomeIcon icon={faArrowRightFromBracket} size='xl' className=' me-3 ' /> Logout </MenuItem>
             </Menu>
         </Sidebar>
     )
--- src/pages/Adminpanel.jsx
+++ src/pages/Adminpanel.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SideControls from '../components/SideControls'
+import Inbox from '../components/Inbox'
+import Overview from '../components/Overview'
 
 function Adminpanel() {
+
+  
   return (
     <>
-    <div className="container-fluid w-100 m-0 p-0 d-flex" >
-      <SideControls/>
-      <div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt placeat explicabo accusamus, laudantium tempora eius facere ullam cumque necessitatibus expedita rem rerum aut alias hic corrupti recusandae molestias est quia sequi! Id, saepe explicabo? Id aperiam molestiae ducimus at, quaerat possimus esse fuga, consequatur est debitis voluptatum eum libero dolorum porro repellat perferendis et dolores itaque, nemo expedita sit voluptas. Quos laboriosam quas eaque tempore facilis rerum unde sequi quo asperiores excepturi beatae consequuntur aliquam voluptates alias deserunt error et, maxime qui suscipit temporibus soluta placeat! Quis, animi id, ea quas assumenda praesentium sapiente unde omnis amet eos laboriosam eius.</p>
+    <div className=" w-100 m-0 p-0 d-flex" style={{backgroundColor:'#101010'}}>
+      <SideControls />
+      <div className='mt-5 w-100'>
+        {/* <Inbox/> */}
+        <Overview/>
       </div>
     </div>
     </>
