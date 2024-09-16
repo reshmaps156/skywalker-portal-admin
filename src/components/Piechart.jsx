ceeafceb6f18e6f62be432eef8e67da364dba1c1
Reshma PS <reshmaps156@gmail.com>
2024-09-16 16:12:46 +0530

    data fetched

--- src/components/Piechart.jsx
+++ src/components/Piechart.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { PieChart } from '@mui/x-charts/PieChart';
+import { userRequestContext } from '../utils/Datashare';
 function Piechart() {
+    const {userRequest} = useContext(userRequestContext)
+    const totalHigh = userRequest.filter(req=>req.priority=='High').length
+    const totalMedium = userRequest.filter(req=>req.priority=='Medium').length
+    const totalLow = userRequest.filter(req=>req.priority=='Low').length
+  
   return (
     <PieChart 
     colors={['red,orange,yellow']}
       series={[
         {
           data: [
-            { id: 0, value: 10, label: 'High',color:'red'},
-            { id: 1, value: 15, label: 'Medium',color:'orange' },
-            { id: 2, value: 20, label: 'Low',color:'yellow' },
+            { id: 0, value: totalHigh, label: 'High',color:'red'},
+            { id: 1, value: totalMedium, label: 'Medium',color:'orange' },
+            { id: 2, value: totalLow, label: 'Low',color:'yellow' },
           ],
         },
       ]}
